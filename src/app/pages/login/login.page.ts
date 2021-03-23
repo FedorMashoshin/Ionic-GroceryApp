d7d1344e2b77a43b6cb12f5afba07262a921bd18
FedorMashoshin <fmashoshin@gmail.com>
2021-03-23 09:56:19 -0700

    Basic logic to login page

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  registerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      name: ['', Validators.required],
+      type: ['BUYER', Validators.required]
+    })
+  }
+
+}
