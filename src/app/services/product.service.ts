1d7140f729036ec120a3a58411d9fcbfc2dc6401
FedorMashoshin <fmashoshin@gmail.com>
2021-04-07 20:46:35 -0700

    Add functionality to upload product with image

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,29 @@ export class ProductService {
   constructor(
     private angularFireStore: AngularFirestore,
     private angularFireAuth: AngularFireAuth,
+    private storage: AngularFireStorage
   ) { }
 
   public async addProduct(product){
-    product.creator = (await this.angularFireAuth.currentUser).uid
-    return this.angularFireStore.collection('products').add(product);
+    product.creator = (await this.angularFireAuth.currentUser).uid;
+    const imageData = product.img;
+    delete product.image;
+
+    let documentId = null;
+    let storageRef: AngularFireStorageReference = null;
+
+    return this.angularFireStore.collection('products').add(product).then(ref => {
+      console.log('ref: ', ref);
+      documentId = ref.id;
+      storageRef = this.storage.ref(`products/${documentId}`);
+      const uploadTask = storageRef.putString(imageData, 'base64', { contentType: 'image/png' });
+      return uploadTask;
+    }).then(task => {
+      console.log('TASK: ', task);
+      return storageRef.getDownloadURL().toPromise()
+    }).then(imageUrl => {
+      console.log('URL: ', imageUrl);
+      return this.angularFireStore.doc(`products/${documentId}`).update({ img: imageUrl });
+    });
   }
 }
